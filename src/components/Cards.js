6ecaeffcd30a280c4ce2c74aead987c58b4e3bd4
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-03-06 11:54:30 -0300

    finalizado cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,66 +3,62 @@ import { useState } from "react";
 import styled from "styled-components";
 
 
-export default function Cards(props){
+export default function Cards(props) {
 
-   const {question,answer} = props.elemento
+   const { question, answer } = props.elemento
    const indice = props.indice
-   const [controle,setControle]=useState(0)
-   const [final,setFinal]=useState('assets/seta_play.png')
-   const [cor,setCor]=useState('#333333')
-   const [ativo,setAtivo]=useState(false)
+   const [controle, setControle] = useState(0)
+   const [final, setFinal] = useState('assets/seta_play.png')
+   const [cor, setCor] = useState('#333333')
+   const [ativo, setAtivo] = useState(false)
 
-return(
-   <>
-<DivCard 
-ativo={ativo}
- cor={cor}
-  final={final}
-   controle={controle}
-    setControle={setControle}>
+   return (
+      <>
+         <DivCard
+            ativo={ativo}
+            cor={cor}
+            final={final}
+            controle={controle}
+            setControle={setControle}>
 
-   <div>
-   <p>{controle}Pergunta {indice+1}</p>
-   <button disabled={ativo} onClick={()=>setControle(1)}>
-      <img src={final}></img>
-   </button>
-   </div>
+            <div>
+               <p>Pergunta {indice + 1}</p>
+               <button disabled={ativo} onClick={() => setControle(1)}>
+                  <img src={final}></img>
+               </button>
+            </div>
 
-</DivCard>
+         </DivCard>
 
-<Pergunta controle={controle} setControle={setControle}>
-<p>{question}</p>
-<div>
-<button onClick={()=>setControle(2)}><img src="assets/seta_virar.png"></img></button>
-</div>
-</Pergunta>
-<Resposta setAtivo={setAtivo} setCor={setCor} setFinal={setFinal} controle={controle} setControle={setControle} >
-   <p>{answer}</p>
-   <div>
-      <button onClick={()=> (setFinal('assets/icone_erro.png'),setControle(0),setCor('#FF3030'),setAtivo(true)) }>Não lembrei</button>
-      <button onClick={()=> (setFinal('assets/icone_quase.png'),setControle(0),setCor('#FF922E'),setAtivo(true)) }>Quase não lembrei</button>
-      <button onClick={()=> (setFinal('assets/icone_certo.png'),setControle(0),setCor('#2FBE34'),setAtivo(true) )}>Zap!</button>
-   </div>
-</Resposta>
-</>
-)
+         <Pergunta controle={controle} setControle={setControle}>
+            <p>{question}</p>
+            <div>
+               <button onClick={() => setControle(2)}><img src="assets/seta_virar.png"></img></button>
+            </div>
+         </Pergunta>
+         <Resposta setAtivo={setAtivo} setCor={setCor} setFinal={setFinal} controle={controle} setControle={setControle} >
+            <p>{answer}</p>
+            <div>
+               <button onClick={() => (setFinal('assets/icone_erro.png'), setControle(0), setCor('#FF3030'), setAtivo(true))}>Não lembrei</button>
+               <button onClick={() => (setFinal('assets/icone_quase.png'), setControle(0), setCor('#FF922E'), setAtivo(true))}>Quase não lembrei</button>
+               <button onClick={() => (setFinal('assets/icone_certo.png'), setControle(0), setCor('#2FBE34'), setAtivo(true))}>Zap!</button>
+            </div>
+         </Resposta>
+      </>
+   )
 }
 
 
 
-
-
-const Resposta=styled.div`
+const Resposta = styled.div`
 width: 300px;
-
 max-height: 100%;
 background-color: #FFFFD4;
-display: ${(props) => (props.controle === 2?'flex':'none')};
+display: ${(props) => (props.controle === 2 ? 'flex' : 'none')};
 flex-direction: column;
 justify-content: space-between;
 
 p{
-
 padding-left: 15px;
 padding-right: 15px;
 padding-top: 15px;
@@ -72,7 +68,6 @@ font-weight: 400;
 line-height: 22px;
 letter-spacing: 0em;
 text-align: left;
-
 }
  
 div{
@@ -109,22 +104,18 @@ div :nth-child(2){
 div :nth-child(3){
    background-color: #2FBE34;
 }  
-
-
-
 `
 
 
-const Pergunta= styled.div`
+const Pergunta = styled.div`
 width: 300px;
 min-height: 131px;
 background-color: #FFFFD4;
-display: ${(props) => (props.controle===1?'flex':'none')};
+display: ${(props) => (props.controle === 1 ? 'flex' : 'none')};
 flex-direction: column;
 justify-content: space-between;
 
 p{
-   
 font-family: 'Recursive';
 font-size: 18px;
 font-weight: 400;
@@ -151,8 +142,7 @@ padding-bottom: 3px;
 const DivCard = styled.div`
 width: 300px;
 min-height: 65px;
-
-display: ${(props) => (props.controle===0?'flex':'none')};
+display: ${(props) => (props.controle === 0 ? 'flex' : 'none')};
 justify-content: space-between;
 align-items: center;
 padding-left: 15px;
@@ -178,8 +168,8 @@ font-weight: 700;
 line-height: 19px;
 letter-spacing: 0em;
 text-align: left;
-color:${(props)=>(props.cor)};
-
+color:${(props) => (props.cor)};
+text-decoration:${(props) => props.ativo ? 'line-through' : 'none'};
 }
 
 div{
